8a9c90beab463c3d74e5ab6aeb8e0adb8f83c621
kellivalverde <paughka@gmail.comls>
2020-01-05 14:06:05 -0500

    swap 2 chars and then some with tests

--- src/SwapChars.java
+++ src/SwapChars.java
@@ -2,8 +2,19 @@
 public class SwapChars {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// given a String, swap and return the last 2 characters of the string
+		//use TDD
+		
+		
 
+	}
+
+	public static String swap2(String str) {
+		// TODO Auto-generated method stub
+		char char1 = str.charAt(0);
+		char char2 = str.charAt(1);
+		
+		return "" + char2 + char1;  // quotes let it know we want a String
 	}
 
 }
---
+++ src/SwapCharsTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SwapCharsTest {
+
+	@Test
+	void swapsStringsWith2Chars() {
+		assertEquals("AB",SwapChars.swap2("BA"));  // didn't make SwapChars2 yet --> fail -> makes a new method
+		
+		
+	} //void swap2
+
+	void swapsStringsWith6Chars() {
+		assertEquals("AB",SwapChars.swap2("BA"));  // didn't make SwapChars2 yet --> fail -> makes a new method
+		
+		
+	
+	
+	
+} //class
